f334d9d98c1a37cbd91f0845d98f2b7ccc1c2b0a
stefanusagranus-tech <stefanusagranus@gmail.com>
2026-08-11 06:17:58 +0700

    Remove unnecessary closing tags in HTML
    Removed closing tbody and table tags from HTML.

--- app.py
+++ app.py
@@ -678,8 +678,6 @@ elif selected_tab == "02 · Detail Item":
                     </thead>
                     <tbody>
                         {table_rows_html}
-                    </tbody>
-                </table>
             </div>
         """, unsafe_allow_html=True)
 
