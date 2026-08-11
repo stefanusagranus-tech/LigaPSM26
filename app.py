23ee6baba0b1816b8874b80d08cc9daf3b6e32d6
stefanusagranus-tech <stefanusagranus@gmail.com>
2026-08-11 08:07:10 +0700

    Fix indentation for selected_tab condition

--- app.py
+++ app.py
@@ -503,7 +503,7 @@ st.markdown(f"""
 """, unsafe_allow_html=True)
 
 # --- TAB 01: OVERVIEW PENJUALAN (TIME FACTOR & FILTER TANGGAL) ---
-elif selected_tab == "01 · Overview Penjualan":
+    elif selected_tab == "01 · Overview Penjualan":
     st.title("📊 Overview Penjualan Toko")
     
     si_df = st.session_state.sales_item_df.copy()
