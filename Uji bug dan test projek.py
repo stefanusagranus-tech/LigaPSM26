ea9cd3e03c0301d410b5e36a0d7831cfcdb52df8
stefanusagranus-tech <stefanusagranus@gmail.com>
2026-08-27 13:42:45 +0700

    Import timedelta from datetime module

--- Uji bug dan test projek.py
+++ Uji bug dan test projek.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 from streamlit_gsheets import GSheetsConnection
 
