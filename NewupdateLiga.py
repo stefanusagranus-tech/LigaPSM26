4434950e2a537ffa163199877cd13e25f5a1a6b4
stefanusagranus-tech <stefanusagranus@gmail.com>
2026-09-04 19:31:35 +0700

    Rename CSS classes for RPG bars in NewupdateLiga.py

--- NewupdateLiga.py
+++ NewupdateLiga.py
@@ -971,21 +971,21 @@ if "portal_prep_ready" in st.session_state and st.session_state.portal_prep_read
                         <div class="rpg-stat-container">
                             <div class="rpg-stat-header"><span>🔥 ATTACK (PENCAPAIAN PSM)</span><span>""" + str(test_qty_psm) + """ Qty</span></div>
                             <div class="rpg-bar-bg">
-                            <div class="rpg-bar-fill-psm" style="width: """ + str(test_percent_psm) + """; height: 100%; border-radius: 4px; display: block; background: linear-gradient(90deg, #ef4444, #ff8080) !important;"></div>
+                            <div class="rpg_bar_fill_psm" style="width: """ + str(test_percent_psm) + """; height: 100%; border-radius: 4px; display: block; background: linear-gradient(90deg, #ef4444, #ff8080) !important;"></div>
                             </div>
                         </div>
                         <!-- BAR pps -->
                         <div class="rpg-stat-container">
                             <div class="rpg-stat-header"><span>🛡️ DEFENSE (PENJUALAN PPS)</span><span>""" + str(test_qty_pps) + """ Poin</span></div>
                             <div class="rpg-bar-bg">
-                            <div class="rpg-bar-fill-pps" style="width: """ + str(test_percent_pps) + """; height: 100%; border-radius: 4px; display: block;"></div>
+                            <div class="rpg_bar_fill_pps" style="width: """ + str(test_percent_pps) + """; height: 100%; border-radius: 4px; display: block;"></div>
                             </div>
                         </div>
                         <!-- BAR sueger -->
                         <div class="rpg-stat-container">
                             <div class="rpg-stat-header"><span>🍃 AGILITY (PENJUALAN SUEGER)</span><span>""" + str(test_qty_sueger) + """ Qty</span></div>
                             <div class="rpg-bar-bg">
-                            <div class="rpg-bar-fill-sueger" style="width: """ + str(test_percent_sueger) + """; height: 100%; border-radius: 4px; display: block;"></div>
+                            <div class="rpg_bar_fill_sueger" style="width: """ + str(test_percent_sueger) + """; height: 100%; border-radius: 4px; display: block;"></div>
                             </div>
                         </div>
                         <div class="avatar-holder-bottom">👤</div>
