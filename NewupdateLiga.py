f28f62f74b92374a97eff8442ba17d730055c4eb
stefanusagranus-tech <stefanusagranus@gmail.com>
2026-09-10 06:11:13 +0700

    test rasa

--- NewupdateLiga.py
+++ NewupdateLiga.py
@@ -2687,6 +2687,7 @@ if "portal_prep_ready" in st.session_state and st.session_state.portal_prep_read
         # 📄 5. KONTEN PER HALAMAN BUKU (Halaman 1)
         # ==========================================
         if page_num == 1:
+            st.error("🚨 DEBUG: Halaman 1 sedang dirender!") # <--- INI UNTUK MENGECEK APAKAH INI MUNCUL 2 KALI
             periods_df = st.session_state.get("periods_df", pd.DataFrame())
             target_period_id = ""
             if not periods_df.empty:
