ee90a3a0ab93b761329b44787253f44fff20e6de
stefanusagranus-tech <stefanusagranus@gmail.com>
2026-09-10 08:20:37 +0700

    spasi sialan

--- NewupdateLiga.py
+++ NewupdateLiga.py
@@ -2846,7 +2846,7 @@ if "portal_prep_ready" in st.session_state and st.session_state.portal_prep_read
             </div>
             """
         
-       elif page_num == 3:
+        elif page_num == 3:
             # --- STYLING CSS RPG BADGE FRAME & UI (WATERMARK NAGA PROPORSIONAL & TERANG) ---
             rpg_badge_style = """
             <style>
