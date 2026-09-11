7c44708103c0c071844f6120ab85e192e1c07b0a
stefanusagranus-tech <stefanusagranus@gmail.com>
2026-09-11 19:48:56 +0700

    lll

--- NewupdateLiga.py
+++ NewupdateLiga.py
@@ -2970,6 +2970,7 @@ if "portal_prep_ready" in st.session_state and st.session_state.portal_prep_read
                 </div>
             </div>
             """
+            st.markdown(html_open_tugas, unsafe_allow_html=True)
 
         elif page_num == 3:
             # --- STYLING CSS RPG BADGE FRAME & UI (WATERMARK NAGA PROPORSIONAL & TERANG) ---
