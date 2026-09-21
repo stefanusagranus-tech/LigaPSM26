0804f76e78dd60db48a1906f060ebb3faef8397c
stefanusagranus-tech <stefanusagranus@gmail.com>
2026-09-22 05:56:24 +0700

    lll

--- NewupdateLiga.py
+++ NewupdateLiga.py
@@ -51,6 +51,7 @@ from spreadsheet_connector import (
     generate_laporan_sg,
     generate_laporan_sueger,
     generate_semua_laporan,
+    isi_laporan_psm,
     
     # Heartbeat (FASE 4)
     write_heartbeat_to_sheet,
